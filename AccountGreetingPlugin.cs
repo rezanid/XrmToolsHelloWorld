a8bd130ebe4915f31e520213be63716ae101998a
Reza <reza.niroomand@hotmail.com>
2025-11-02 17:55:42 +0100

    Xrm Tools Hello World Begins

---
+++ AccountGreetingPlugin.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace XrmToolsHelloWorld
+{
+    public partial class AccountGreetingPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var target = (Entity)context.InputParameters["Target"];
+            target["description"] = "Hello from XrmToolsHelloWorld Plugin!"];
+        }
+    }
+}
